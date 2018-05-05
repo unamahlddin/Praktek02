1b47e4540be2e86c3eb42bf6658e423153676d3e
unamahlddin <https://github.com/unamahlddin>
2018-05-05 10:18:47 +0800

    Membuat Attribute dan Behavior

--- src/praktek02/Triangle.java
+++ src/praktek02/Triangle.java
@@ -1,4 +1,14 @@
 package praktek02;
 public class Triangle {
-
+    String alas;
+    String tinggi;
+    String lebar;
+    
+    void cetakInfo(){
+        System.out.println ("==================");
+        System.out.println ("alas : "+alas);
+        System.out.println ("tinggi : "+tinggi);
+        System.out.println ("lebar : "+lebar);
+        System.out.println ("==================");
+    }
 }
